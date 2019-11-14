f94e8d2b14dd40f3490815137ae8eeceeb2cb647
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-11-13 19:54:50 -0800

    adding notes to the Card component and Footer component

--- Google Books /client/src/components/Card/index.js
+++ Google Books /client/src/components/Card/index.js
@@ -1,18 +1,25 @@
+// import React within current scope
 import React from "react";
 
+
+// functional component called Card that is passed the icon, title, and children props
 function Card({ icon, title, children }) {
   return (
     <div className="card mt-4">
       <div className="card-header">
         <h3>
           <strong>
+            {/* bold i tag passed in the icon prop as the className and the title prop */}
             <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
           </strong>
         </h3>
       </div>
+      {/* passing in the children prop as the body of the card.  */}
+      {/* This will populate whatever is set as the children prop into the body of the card */}
       <div className="card-body">{children}</div>
     </div>
   );
 }
 
+// export the Card component
 export default Card;
--- Google Books /client/src/components/Footer/index.js
+++ Google Books /client/src/components/Footer/index.js
@@ -1,7 +1,10 @@
+// import React
 import React from "react";
 
+// functioncal component called Footer and it is not passed any props so it will just have hard coded content
 function Footer() {
   return (
+    // JSX footer 
     <footer>
       <hr />
       <p className="pull-right">
@@ -11,4 +14,5 @@ function Footer() {
   );
 }
 
+// export footer
 export default Footer;
